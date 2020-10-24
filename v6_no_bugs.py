bbc19d9b156f90f89d8162aad213b1e31ee0eca6
teamrejects <66080061+teamrejects@users.noreply.github.com>
2020-10-24 13:05:49 +0530

    Update v6_no_bugs.py

--- v6_no_bugs.py
+++ v6_no_bugs.py
@@ -11,10 +11,14 @@ root.title("ARK CS")
 root.minsize(width=350, height=550) 
 laps = []
 L =  []
- 
+t_counter = 0
 import time
-
+t_list = []
+r=[]
+T = 0
+t_list2 = []
 #____________________________________________________________________________________________________________________________________________________________________
+
 
 
 # STUDY TIME TEXT
@@ -26,12 +30,13 @@ def study_time(text):
     T1.configure(state="disabled")
 study_time("Study Clock :")
 
-"complete checklists! to get more break time to browse the blocked websites =>"
 
 #CLOCK1
-def counter_label1(label1):  #clock1
+def counter_label1(label1 , t_counter):  #clock1
     def count1():  
-        if running1:  
+        if running1:
+            
+            global t_counter
             global counter1    
             if counter1==0:              
                 display1=""
@@ -44,13 +49,17 @@ def counter_label1(label1):  #clock1
             label1['text']=display1   
             label1.after(1000, count1)   
             counter1 += 1
-    count1()       
+            t_counter += 1
+            return t_counter
+            
+    count1()
+
 def Start1(label1):          #start button of clock 1 and stop button of clock2
     global running1 
     global running 
     running1=True
     running = False
-    counter_label1(label1)  
+    counter_label1(label1 , t_counter)  
     start1['state']='disabled'
     start['state']='normal'
 
@@ -76,23 +85,25 @@ break_time("Break Timer :")
 
 
 #CLOCK2
-def counter_label(label):    #clock2
+def counter_label(label , T):    #clock2
     def count():  
         if running:  
             global counter
-            tt = datetime.fromtimestamp(counter+120) # T = time to be added in the timer
+            tt = datetime.fromtimestamp(counter+T) # T = time to be added in the timer
             string = tt.strftime("%H:%M:%S") 
             display=string  
             label['text']=display   
             label.after(1000, count)   
             counter -= 1
+            if counter == 66600:
+                Start1(label1)
     count()       
 def Start(label):            #start button of clock2 and stop button of clock2
     global running 
     global running1 
     running=True
     running1 = False
-    counter_label(label)  
+    counter_label(label ,T)  
     start['state']='disabled'
     start1['state']='normal'
  
@@ -131,20 +142,18 @@ def checklists():
         n3=var3.get()
         def limit1():
             if n1==1:
-                Tkinter.Label(fc,text=" 1st  checked",fg="green").grid(row=0,column=1)
-            else:
-                Tkinter.Label(fc,text="1st unchecked",fg="red").grid(row=0,column=1)
-                print
+                t_list.append(t_counter)
+                calc_T(t_list)
+            
         def limit2():
             if n2==1:
-                Tkinter.Label(fc,text=" 2nd  checked",fg="green").grid(row=1,column=1)
-            else:
-                Tkinter.Label(fc,text="2nd unchecked",fg="red").grid(row=1,column=1)
+                t_list.append(t_counter)
+                calc_T(t_list)
+
         def limit3():
             if n3==1:
-                Tkinter.Label(fc,text=" 3rd  checked",fg="green").grid(row=2,column=1)
-            else:
-                Tkinter.Label(fc,text="3rd unchecked",fg="red").grid(row=2,column=1)
+                t_list.append(t_counter)
+                calc_T(t_list)
         limit1()
         limit2()
         limit3()
@@ -162,28 +171,23 @@ def close_window():
     L.append(entry)
     
 
+def calc_T(t_list ):
+    t_list2 = []
+    global T
+    for i in range(1, len(t_list)): 
+        t_list2.append((t_list[i] - t_list[i-1])/3) 
+    for ele in range(0, len(t_list2)): 
+        T = T + t_list2[ele]
+    
+    return T 
+
 E = Tkinter.Entry(root , highlightthickness = 0 ,borderwidth=0)
 E.pack(anchor = CENTER)
 B = Button(root, text = "Enter the websites \n that distract you.", command = close_window )
 B.pack(anchor = S)
 
 
-#bugs / features to be added
-# the stopwatch and timer should only work if websites to be blocked have been entered 
-# when the timer runs out it starts from 23:59:59 , instead when timer times out (counter = 66600) then study clock should start
-# we need to make a button to add more checklists
-# the stopwatch and timer should only work if websites to be blocked have been entered 
-# when the timer runs out it starts from 23:59:59 , instead when timer times out (counter = 66600) then study clock should start
-# we need to make a button to add more checklists
-# disable update button when redeem timer is going on
-# everytime the checklist button is clicked , we lap the time it took to click that button in reference to study clock 
-# then 1/3 of the study clock lap time is added to break timer
-# if checkbox gets checked it should not be able to get unchecked
-#the checklist should be updated on clikcing so no update button should be required
-
-# EXIT BUTTON
 b2 = Tkinter.Button(root, text = "Exit", 
             command = root.destroy)
 b2.pack(anchor = 'se')
-root.mainloop() 
-print(L)
+root.mainloop()
